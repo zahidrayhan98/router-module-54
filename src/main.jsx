7c82457c8c956b2400bc5290f225bd847dd7a783
zahidrayhan98 <zahid.rayhan1848@gmail.com>
2023-04-05 01:31:14 +0600

    link

---
+++ src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import About from './component/About/About';
+import Contact from './component/Contact/Contact';
+import Home from './component/Home/Home';
+import First from './component/First/First';
+import Friends from './component/Friends/Friends';
+//  const router=createBrowserRouter([
+//   {
+//   path:'/',
+//   element: <App></App>
+//  },
+//  {
+//   path:'/about',
+//   element:<About></About>
+//  },
+//  {
+//  path:'/contact',
+//  element:<Contact></Contact>
+//  }
+// ])
+const router=createBrowserRouter([
+  {
+    path:'/',
+    element:<Home></Home>,
+    children:[
+      {
+       path:'/',
+       element:<First></First>
+      },
+      {
+        path:'friends',
+        element:<Friends></Friends>,
+        loader:()=>fetch('https://jsonplaceholder.typicode.com/users')
+      },
+      {
+        path:'about',
+        element:<About></About>
+      },
+      {
+        path:'contact',
+        element:<Contact></Contact>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+ <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>,
+)
